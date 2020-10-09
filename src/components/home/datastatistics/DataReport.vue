fd71428b699f082d566a0ddb38cf305fe69a27e7
liuyifan <2414378666@qq.com>
2020-10-09 23:01:37 +0800

    '数据报表'

--- src/components/home/datastatistics/DataReport.vue
+++ src/components/home/datastatistics/DataReport.vue
@@ -7,19 +7,67 @@
     </el-breadcrumb>
     <!-- 卡片区域 -->
     <el-card>
-
+      <!-- 为ECharts准备一个具备大小（宽高）的Dom -->
+      <div id="main" style="width: 750px;height:400px;"></div>
     </el-card>
   </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+import _ from 'lodash'
+
 export default	{
   name:	'DataReport',
   data() {
     return {
-      
+      //需要合并的数据
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
     }
-  }
+  },
+  async mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'));
+
+    //获取折线图数据
+    const {data: res} = await this.$http.get('reports/type/1')
+    if(res.meta.status != 200) return this.$message.error('折线图数据获取失败')
+
+    //准备数据和配置项 合并数据
+    const result =  _.merge(res.data, this.options)
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(result);
+  },
 }
 </script>
 
